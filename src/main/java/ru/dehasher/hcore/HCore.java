c47c886da658a8fb087698d51b3765ce78c9bd0d
deHasher <deHasher@icloud.com>
2021-03-30 22:02:05 +0500

    bussy <3

--- src/main/java/ru/dehasher/hcore/HCore.java
+++ src/main/java/ru/dehasher/hcore/HCore.java
@@ -262,6 +262,6 @@ public class HCore extends JavaPlugin {
     	getLogger().info("● ║╔╗║║╔╗║║╔═╗║╚ ╗║ ║══╣║╔╗║║╔╗║║╔╝ ●");
     	getLogger().info("● ║╚╝║║║═╣║║ ║║║╚╝╚╗╠══║║║║║║║═╣║║  ●");
     	getLogger().info("● ╚══╝╚══╝╚╝ ╚╝╚═══╝╚══╝╚╝╚╝╚══╝╚╝  ●");
-    	getLogger().info("5");
+    	getLogger().info("6");
     }
 }
