ea5dc47fd957e70d50ff4d6d44bca76c49695465
deHasher <deHasher@icloud.com>
2021-07-07 22:51:57 +0500

    bussy <3

--- src/main/java/ru/dehasher/bukkit/api/tab/TAPI.java
+++ src/main/java/ru/dehasher/bukkit/api/tab/TAPI.java
@@ -4,16 +4,15 @@ import me.neznamy.tab.api.EnumProperty;
 import me.neznamy.tab.api.TABAPI;
 import org.bukkit.entity.Player;
 
-@SuppressWarnings("deprecation")
 public class TAPI {
 
     public static void setPrefix(Player player, String string) {
-        TABAPI.setValuePermanently(player.getUniqueId(), EnumProperty.TABPREFIX, string);
-        TABAPI.setValuePermanently(player.getUniqueId(), EnumProperty.TAGPREFIX, string);
+        TABAPI.getPlayer(player.getUniqueId()).setValuePermanently(EnumProperty.TABPREFIX, string);
+        TABAPI.getPlayer(player.getUniqueId()).setValuePermanently(EnumProperty.TAGPREFIX, string);
     }
 
     public static void setSuffix(Player player, String string) {
-        TABAPI.setValuePermanently(player.getUniqueId(), EnumProperty.TABSUFFIX, string);
-        TABAPI.setValuePermanently(player.getUniqueId(), EnumProperty.TAGSUFFIX, string);
+        TABAPI.getPlayer(player.getUniqueId()).setValuePermanently(EnumProperty.TABSUFFIX, string);
+        TABAPI.getPlayer(player.getUniqueId()).setValuePermanently(EnumProperty.TAGSUFFIX, string);
     }
 }
