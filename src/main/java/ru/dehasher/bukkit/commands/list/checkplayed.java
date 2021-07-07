e9b3906d2c7b88c0efd4ab8cea60395aa343ecff
deHasher <deHasher@icloud.com>
2021-07-08 01:10:20 +0500

    bussy <3

--- src/main/java/ru/dehasher/bukkit/commands/list/checkplayed.java
+++ src/main/java/ru/dehasher/bukkit/commands/list/checkplayed.java
@@ -1,6 +1,5 @@
 package ru.dehasher.bukkit.commands.list;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -8,6 +7,7 @@ import ru.dehasher.bukkit.HCore;
 import ru.dehasher.bukkit.managers.Informer;
 import ru.dehasher.bukkit.managers.Methods;
 
+@SuppressWarnings("deprecation")
 public class checkplayed {
     public static boolean send(CommandSender sender, String command, String[] args) {
         int length    = args.length;
@@ -18,8 +18,8 @@ public class checkplayed {
 
         if (Methods.isPerm(player, "hcore.command.checkplayed")) {
             if (length > 0) {
-                Player target = Bukkit.getPlayer(args[0]);
-                Informer.send(player, (target.hasPlayedBefore()) ? 1 : 0);
+                Player target = (Player) HCore.getPlugin().getServer().getOfflinePlayer(args[0]);
+                Informer.send(player, target.hasPlayedBefore() ? 1 : 0);
                 return true;
             } else {
                 Informer.send(player, info);
