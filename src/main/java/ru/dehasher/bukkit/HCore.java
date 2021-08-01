1768bf92ee3f51e297f94788761d92e393ac94b6
deHasher <dehasher@icloud.com>
2021-08-01 05:09:57 +0500

    bussy <3

--- src/main/java/ru/dehasher/bukkit/HCore.java
+++ src/main/java/ru/dehasher/bukkit/HCore.java
@@ -35,7 +35,7 @@ public class HCore extends JavaPlugin {
     public static String main_file       = "main.yml";
     public static String spawn_file      = "spawn.yml";
     public static Double main_version    = 0.1;
-    public static Double lang_version    = 1.43;
+    public static Double lang_version    = 1.44;
     public static Double config_version  = 1.98;
     public static Boolean disable_bypass = false;
 
