4da3c3a752c3d786e1ff6e1668487fbb70c39daa
deHasher <dehasher@icloud.com>
2021-09-02 20:24:15 +0500

    bussy <3

--- src/main/java/ru/dehasher/bukkit/commands/list/prefix.java
+++ src/main/java/ru/dehasher/bukkit/commands/list/prefix.java
@@ -17,120 +17,106 @@ public class prefix {
         Player player     = null;
         String playerName = HCore.lang.getString("console-name");
         String info       = Methods.getCommandInfo(command);
-        int    length     = message.length;
+        int length        = message.length;
         int max_chars     = HCore.config.getInt("send-command.plugin.commands.prefix.data.max-limit");
 
-
         if (sender instanceof Player) {
-            player = (Player) sender;
+            player     = (Player) sender;
             playerName = player.getName();
         }
 
-        if (Methods.isPerm(player, "hcore.command.prefix")) {
-            if (length > 2) {
+        if (!Methods.isPerm(player, "hcore.command.prefix")) {
+            Informer.send(player, HCore.lang.getString("errors.no-perm"));
+            return false;
+        }
 
-                String type    = message[1].toLowerCase();
-                Player target  = HCore.getPlugin().getServer().getPlayer(message[2]);
+        if (length > 2) {
+            String type    = message[1].toLowerCase();
+            Player target  = HCore.getPlugin().getServer().getPlayer(message[2]);
 
-                if (target != null && target.isOnline()) {
-                    if (type.equals("clear")) {
-                        if (Methods.checkPlugin(Plugins.LuckPerms)) {
-                            LPAPI.reset(target, "prefix");
-                            LPAPI.reset(target, "suffix");
-                        } else {
-                            Informer.send(player, HCore.lang.getString("errors.no-plugin")
-                                    .replace("{plugin}", "LuckPerms")
-                            );
+            if (target == null || target.isOnline()) {
+                Informer.send(player, HCore.lang.getString("errors.player-not-found"));
+                return false;
+            }
+
+            switch (type) {
+                case "clear":
+                    if (Methods.checkPlugin(Plugins.LuckPerms)) {
+                        LPAPI.reset(target, "prefix");
+                        LPAPI.reset(target, "suffix");
+                    } else {
+                        Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "LuckPerms"));
+                        return false;
+                    }
+                    if (Methods.checkPlugin(Plugins.TAB)) {
+                        TAPI.setPrefix(target, "");
+                        TAPI.setSuffix(target, "");
+                    } else {
+                        Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "TAB"));
+                        return false;
+                    }
+                    Informer.send(player, HCore.lang.getString("commands.prefix.clear.self").replace("{player}", target.getName()));
+                    if (target != player) Informer.send(target, HCore.lang.getString("commands.prefix.clear.target").replace("{player}", playerName));
+                    return true;
+                case "tab":
+                case "chat":
+                    if (length > 3) {
+                        String prefix = Methods.colorSet(fix(message[3]));
+                        String suffix = "";
+
+                        if (message[3].contains("<name>")) {
+                            String[] msg = message[3].split("<name>", 2);
+                            prefix = Methods.colorSet(fix(msg[0]));
+                            if (msg.length > 1) suffix = Methods.colorSet(fix(msg[1]));
+                        }
+
+                        String prefixSpace = (Methods.colorClear(prefix).length() > 0 && !Methods.colorClear(prefix).endsWith(" "))   ? " " : "";
+                        String suffixSpace = (Methods.colorClear(suffix).length() > 0 && !Methods.colorClear(suffix).startsWith(" ")) ? " " : "";
+
+                        if (Methods.colorClear(prefix).length() > max_chars) {
+                            Informer.send(player, HCore.lang.getString("commands.prefix.error").replace("{max}", "" + max_chars));
                             return false;
                         }
-                        if (Methods.checkPlugin(Plugins.TAB)) {
-                            TAPI.setPrefix(target, "");
-                            TAPI.setSuffix(target, "");
-                        } else {
-                            Informer.send(player, HCore.lang.getString("errors.no-plugin")
-                                    .replace("{plugin}", "TAB")
-                            );
+
+                        if (Methods.colorClear(suffix).length() > max_chars) {
+                            Informer.send(player, HCore.lang.getString("commands.prefix.error").replace("{max}", "" + max_chars));
                             return false;
                         }
-                        Informer.send(player, HCore.lang.getString("commands.prefix.clear.self")
-                                .replace("{player}", target.getName())
-                        );
-                        if (target != player) {
-                            Informer.send(target, HCore.lang.getString("commands.prefix.clear.target")
-                                    .replace("{player}", playerName)
-                            );
-                        }
-                        return true;
-                    } else if (type.equals("chat") || type.equals("tab")) {
-                        if (length > 3) {
-                            String prefix = Methods.colorSet(fix(message[3]));
-                            String suffix = "";
 
-                            if (message[3].contains("<name>")) {
-                                String[] msg = message[3].split("<name>", 2);
-                                prefix = Methods.colorSet(fix(msg[0]));
-                                if (msg.length > 1) {
-                                    suffix = Methods.colorSet(fix(msg[1]));
-                                }
-                            }
-
-                            String prefixSpace = (Methods.colorClear(prefix).length() > 0 && !Methods.colorClear(prefix).endsWith(" "))   ? " " : "";
-                            String suffixSpace = (Methods.colorClear(suffix).length() > 0 && !Methods.colorClear(suffix).startsWith(" ")) ? " " : "";
-
-                            if (Methods.colorClear(prefix).length() > max_chars) {
-                                Informer.send(player, HCore.lang.getString("commands.prefix.error")
-                                        .replace("{max}", "" + max_chars));
-                                return false;
-                            }
-
-                            if (Methods.colorClear(suffix).length() > max_chars) {
-                                Informer.send(player, HCore.lang.getString("commands.prefix.error")
-                                        .replace("{max}", "" + max_chars));
-                                return false;
-                            }
-
-                            if (type.equals("chat")) {
-                                if (Methods.checkPlugin("LuckPerms")) {
-                                    LPAPI.setPrefix(target, prefix + prefixSpace);
-                                    LPAPI.setSuffix(target, suffixSpace + suffix);
-                                } else {
-                                    Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "LuckPerms"));
-                                    return false;
-                                }
+                        if (type.equals("chat")) {
+                            if (Methods.checkPlugin("LuckPerms")) {
+                                LPAPI.setPrefix(target, prefix + prefixSpace);
+                                LPAPI.setSuffix(target, suffixSpace + suffix);
                             } else {
-                                if (Methods.checkPlugin("TAB")) {
-                                    TAPI.setPrefix(target, prefix + prefixSpace);
-                                    TAPI.setSuffix(target, suffixSpace + suffix);
-                                } else {
-                                    Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "TAB"));
-                                    return false;
-                                }
+                                Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "LuckPerms"));
+                                return false;
                             }
-                            Informer.send(player, HCore.lang.getString("commands.prefix.success.self")
+                        } else {
+                            if (Methods.checkPlugin("TAB")) {
+                                TAPI.setPrefix(target, prefix + prefixSpace);
+                                TAPI.setSuffix(target, suffixSpace + suffix);
+                            } else {
+                                Informer.send(player, HCore.lang.getString("errors.no-plugin").replace("{plugin}", "TAB"));
+                                return false;
+                            }
+                        }
+                        Informer.send(player, HCore.lang.getString("commands.prefix.success.self")
                                 .replace("{type}", HCore.lang.getString("commands.prefix.types." + type))
                                 .replace("{player}", target.getName())
                                 .replace("{prefix}", prefix + prefixSpace + target.getName() + suffixSpace + suffix)
-                            );
-                            if (target != player) {
-                                Informer.send(target, HCore.lang.getString("commands.prefix.success.target")
+                        );
+                        if (target != player) {
+                            Informer.send(target, HCore.lang.getString("commands.prefix.success.target")
                                     .replace("{type}", HCore.lang.getString("commands.prefix.types." + type))
                                     .replace("{player}", playerName)
                                     .replace("{prefix}", prefix + prefixSpace + target.getName() + suffixSpace + suffix)
-                                );
-                            }
-                            return true;
+                            );
                         }
+                        return true;
                     }
-                    Informer.send(player, info);
-                } else {
-                    Informer.send(player, HCore.lang.getString("errors.player-not-found"));
-                }
-            } else {
-                    Informer.send(player, info);
             }
-        } else {
-                Informer.send(player, HCore.lang.getString("errors.no-perm"));
         }
+        Informer.send(player, info);
         return false;
     }
 
